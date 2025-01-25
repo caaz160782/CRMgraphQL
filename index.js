c26d0a0599f45008ff3fcd346f7d6adcc4a59592
carlos <caaz160782@gmail.com>
2025-01-25 12:54:53 -0600

    crm clients

--- index.js
+++ index.js
@@ -2,13 +2,32 @@ const { ApolloServer } = require("apollo-server");
 const  typeDefs = require("./db/schema");
 const  resolvers = require("./db/resolvers");
 const  conectarDBMongo = require("./config/db");
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 //conectar bd
 conectarDBMongo();
 //servidor
 const server = new ApolloServer({
     typeDefs,
-    resolvers
+    resolvers,
+    context: ({req}) => {
+        // console.log(req.headers['authorization'])
+        // console.log(req.headers);
+        const token = req.headers['authorization'] || '';
+        if(token) {
+            try {
+                const usuario = jwt.verify(token.replace('Bearer ', ''), process.env.SECRETA );
+                // console.log(usuario);
+                return {
+                    usuario
+                }
+            } catch (error) {
+                console.log('Hubo un error');
+                console.log(error);
+            }
+        }
+    }
 });
 
 //arrancar el servidor
